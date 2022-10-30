bc003d22f1c4b7be4f3d7304c5795b570842d661
Jason Mason <jasonjaymason@gmail.com>
2022-10-30 13:06:26 -0600

    Alacritty config

--- .config/nvim/lua/jason/lsp/null-ls.lua
+++ .config/nvim/lua/jason/lsp/null-ls.lua
@@ -7,7 +7,7 @@ end
 null_ls.setup({
 	--[[ sources = { ]]
 	--[[ 	require('null-ls').builtins.formatting.stylua, ]]
-	--[[ 	require('null-ls').builtins.formatting.eslint, ]]
+	--[[ require('null-ls').builtins.formatting.eslint, ]]
 	require('null-ls').builtins.formatting.prettier,
 	--[[ }, ]]
 
